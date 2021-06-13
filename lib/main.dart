fc8bbd8fdbbcebd91941cac57acaa73e29a81b4a
SeanPhilippi <sean.philippi@gmail.com>
2021-06-13 17:35:24 -0400

    some buttons and an appbar

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override // explicitly overriding StatelessWidget's built-in build method
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Sup'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Text('The question!'),
+          ElevatedButton(child: Text('Answer 1'), onPressed: null),
+          ElevatedButton(child: Text('Answer 2'), onPressed: null),
+          ElevatedButton(child: Text('Answer 3'), onPressed: null),
+        ]),
+      ),
+    );
   }
 }
