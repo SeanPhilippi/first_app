df3dd33f19060cabaf24c8b5f28e45015e7d06e5
SeanPhilippi <sphilippi@simbex.com>
2021-06-14 15:53:27 -0400

    printing different answers for different buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(String text) {
+    print('Your answer: $text');
+  }
+
   @override // explicitly overriding StatelessWidget's built-in build method
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +16,15 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(children: [
           Text('The question!'),
-          ElevatedButton(child: Text('Answer 1'), onPressed: null),
-          ElevatedButton(child: Text('Answer 2'), onPressed: null),
-          ElevatedButton(child: Text('Answer 3'), onPressed: null),
+          ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion('Answer 1')),
+          ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => answerQuestion('Answer 2')),
+          ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () => answerQuestion('Answer 3')),
         ]),
       ),
     );
